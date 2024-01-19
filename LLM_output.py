6a756d66e102d22e181d820bd4e03445e80c3f68
eddt123 <eddyturner98@gmail.com>
2024-01-19 12:11:06 +0000

    gpt-2 works but the output is bad

--- LLM_output.py
+++ LLM_output.py
@@ -5,13 +5,13 @@ import pandas as pd
 
 #use 15 topics to generate 4 sentences per topic
 
-def generate_sentences(keyword, num_sentences=10):
+def generate_sentences(keyword, topic, num_sentences=1):
     # Load pre-trained model and tokenizer
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large")
     model = GPT2LMHeadModel.from_pretrained("gpt2-large")
 
     # Encode the keyword
-    input_ids = tokenizer.encode(f"generate a sentence of 7-18 words for this word: {keyword}", return_tensors="pt")
+    input_ids = tokenizer.encode(f"generate a sentence of 7-18 words for this word: {topic} it must be related to this topic {keyword}.", return_tensors="pt")
 
     # Set pad_token_id
     model.config.pad_token_id = model.config.eos_token_id
@@ -26,16 +26,27 @@ def generate_sentences(keyword, num_sentences=10):
 
     # Split the text into sentences and return the first `num_sentences`
     sentences = re.split(r'(?<=[.!?]) +', text)
+    for sentence in sentences[:num_sentences]:
+        print(sentence)
+        # Add the sentence to the output file
+        with open(output_file, 'a') as file:
+            file.write(f"{keyword}\t{sentence}\n")
     return sentences[:num_sentences]
 
 
-input_file = os.path.join('wordcloud_sentences.xlsx')
+input_file = os.path.join('data', 'wordcloud_sentences.xlsx')
 output_file = os.path.join('processed_data', 'output.txt')
-data = pd.read_excel(input_file)
-keywords = data.iloc[:, 3].tolist()
+data = pd.read_excel(input_file, usecols=[0, 3], nrows=60)
+keywords = data.iloc[:, 0].tolist()
 
-for keyword in keywords:
-    sentences = generate_sentences(keyword)
+generated_sentences = set()  # To keep track of generated sentences
+
+for keyword, topic in zip(keywords, data.iloc[:, 1]):
+    sentences = generate_sentences(keyword, topic)
     for sentence in sentences:
-        with open(output_file, 'a') as file:
-            file.write(f"{keyword}\t{sentence}\n")
+        # Check if the sentence has already been generated for the keyword and topic
+        if (keyword, sentence) not in generated_sentences:
+            generated_sentences.add((keyword, sentence))
+            # Open the file in append mode to append new content
+            with open(output_file, 'a') as file:
+                file.write(f"{keyword,topic}\t{sentence}\n")
